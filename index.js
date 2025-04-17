503e31776d50858a6c5c724a112b6eb341269642
Nodirbek <shamsiddinovnodirbek469@gmail.com>
2025-04-17 08:50:31 +0500

    add

--- index.js
+++ index.js
@@ -11,26 +11,40 @@ const animalsOptions = {
                 { text: 'Img by ID', callback_data: 'byid' }
             ]
         ]
-    } 
+    }
 }
 
 
 bot.on("message", async (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
-    if (text === "/start") {
-        await bot.sendMessage(chatId, "Bot ish boshladi", animalsOptions);
+    try {
+        if (text === "/start") {
+            await bot.sendMessage(chatId, "Bot ish boshladi", animalsOptions);
+        }
+        if (text >= 100) {
+            bot.sendMessage(chatId, 'Iltimos 99 dan katta son kiritmang')
+        } else {
+            fetch(process.env.IMG_ID_API).then(res => res.json()).then(data =>
+                bot.sendPhoto(chatId, data[text].download_url, animalsOptions)
+            )
+        }
+    } catch (error) {
+        console.log(error)
     }
 });
 bot.on('callback_query', async (msg) => {
     const data = msg.data
     const chatId = msg.message.chat.id;
-    const randomNumber=Math.floor(Math.random()*100)
-    if(data=='random'){ 
+    const randomNumber = Math.floor(Math.random() * 100)
+    if (data == 'random') {
         fetch(process.env.IMG_ID_API).then(res => res.json()).then(data =>
-            bot.sendPhoto(chatId, data[randomNumber].download_url,animalsOptions)
+            bot.sendPhoto(chatId, data[randomNumber].download_url, animalsOptions)
         )
-            
+
     }
-// console.log(data) 
+    if (data === 'byid') {
+        await bot.sendMessage(chatId, "Iltimos 0 dan 99 gacha raqam kiriting");
+    }
+
 })
